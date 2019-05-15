17e0d9689c221e98ff38e2af7eb481dde29860b9
Peerawit-SC <peerawit.sc@gmail.com>
2019-05-15 16:09:02 +0700

    add file

---
+++ create_csv.py
@@ -0,0 +1,41 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "schneider-project",
+  "private_key_id": "ffe6c921bf720c81ac12a07aed76b34a4724403d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDlQUJjdgYIYyio\n3VejDooRZxIp2HGiorrnVcXL9AJ9KFOb7HmLaAvucRfnBbRO/p9uKPbdGVLI52pW\nfUruW7kjbbwOGkcXhx+WLU/4BuCClLshHOVP47MZNvEOku6zIpCCWHsNfi2Rk3NS\nUxpF9IwwH4o7bVFEj3g/h3SSR7TfDRjZ0J6MlnHv3n/UISIPU1HkgxeEvnJ7OKpP\nt0M9rWWoPcrAD7ZUf4usTXpdrfYnpzX3Igx6ImBDPpjt4+21eMqmSkZGZBcIRLe2\nQmoa3liTxGNUBjU59NMP4syaTBUAbCa5zZ5ncM90M/mCsy2baFwF+xT8m3H29FPQ\nUcYyo/qTAgMBAAECggEALf41r3aSJ0Sdd6NQXaohEPS4tCR0R8iV7qtKj2xYw3s/\nqtx6Vfl9y0xtGzANyHtUyOj2/cyCy0OCFe/1BHTqY600OTAUayf0slBM5tmaWRg1\noqMFbk2kNL7NBbt8n/ujOkM+DKrvwXDvheieMMKrDUyGkv2PfE28NiKaxG4WX1kE\nGgOTzYFg9IzlPvX8tF40SPAKraUg/sKreamu5DHp0PJyF1xYXR8eRpzs7lUP9o4W\njWvHQItRSYb6mfbf0yo02ZdBezFxTI7wF1YjYQmdSBZN2aqjPdBSdzCAQh6cMOZr\n+4mawZKn3eYdOsqqEBIj3O8EWqE6Rnmc3t3iEAcIYQKBgQD0MU+Py71BdIbOsJiI\nWrGBhWYtXxK9omFo09Zhe13CF/f/r3mvy4PokSBi0+G5J0u/F6e792e6fAeRgXXe\nlTHj0lL0mDZHPwK8w5ECuxvNIi3F4W+dBOR92I+DAsWpQYtSlesiqRmRIOAdhFDR\nuXhfPWTeIkTUIR8c6AtgUzDtPwKBgQDwVwuTSdIhzWUNgqSlB9T4V41OgUOuimts\nPCXgAlCcu2oa7+firjiNk60P16Q9ZfQ/lf040/0LtcOawk4/h4J7Rq9fps0SaiiT\n8WdI3DJqsyxsYdWBiN+6jQQrZnspKmIYt3D2vHGyDFaGOjM5K+wERz7iAbdUPfbL\na4gI6/eZrQKBgQCdVNwPKNTiRGLK7usIB28QpVNk7jmT5vhd4wbgPez1rh3CJlv/\n0N9HS6ITKrLnaZMbJvGSYN/7PgheTowUAgpn385E+4EVPFrWJibbHJTYASp+sTYJ\nc+qHzq0AWxvhZnKOnjsmVM89O3JKckLkLVyJ+HNxz6CLEGAMbtFNmawPXwKBgQDd\nlBLK7yM5IMkKpKiwscDKWMWhXBGPKHSTa/yrQtcZOLxBK29JEMB43m6OjYwRQx7l\nRJ4Zt2+zTReSZZOIhKFqkCd9cyXb1V2lz8keZkxhDOFm5ufVreA+eGYgsTHZKQcZ\nrH4MLH5R3Ygj5iJ1kiXdZ+X0rgpcM2iF8JSRTSOQXQKBgGcpn7ao2xoszv54apUP\ndhdROLteLrHUPJasTaksTWtFLnxI+5Fi0xz+veiqr3uV4zK1GID3ntC8N968m3Se\naE6telg+HZodjaq3v4/FZ0C6q9fPVwWx7aE8clkTI5Gph6rDV8DW1k0DTftzn3s2\nvIaiTEl9QF/S7oR27363/wqW\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-ygdjt@schneider-project.iam.gserviceaccount.com",
+  "client_id": "103751846783469949126",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ygdjt%40schneider-project.iam.gserviceaccount.com"
+})
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred,
+	{
+    'databaseURL': 'https://schneider-project.firebaseio.com/'
+})
+
+ref = db.reference('energy')
+value = ref.get()
+p1, p2, p3, p4 = [], [], [], []
+i1, i2, i3, i4 = [], [], [], []
+time = []
+for key in value:
+	p1.append(value[key]["P1"])
+	p2.append(value[key]["P2"])
+	p3.append(value[key]["P3"])
+	p4.append(value[key]["P4"])
+
+	i1.append(value[key]["I1"])
+	i2.append(value[key]["I2"])
+	i3.append(value[key]["I3"])
+	i4.append(value[key]["I4"])
+
+	time.append(value[key]["time"])
+
--- rpi_code.py
+++ rpi_code.py
@@ -1,17 +1,78 @@
 from pyModbusTCP.client import ModbusClient
-
+import time
 # TCP auto connect on first modbus request
 c = ModbusClient()
-c.host("192.168.0.102")
+c.host("192.168.0.112")
 c.port(502)
 c.open()
 
+_15minutes = 10*60
 
-c.write_single_coil(0,0) # relay0
-c.write_single_coil(1,0) # relay1
+# c.write_single_coil(0,0) # relay0
+# c.write_single_coil(1,0) # relay1
+# c.write_single_coil(2,0) # relay2
+
+#off all
+print("0")
+	
+c.write_single_coil(1,1) # relay1
 c.write_single_coil(2,1) # relay2
+time.sleep(60)
 
-bits = c.read_coils(0, 3)
+# # 001
+# print("1")
+# c.write_single_coil(0,0) # relay0
+# c.write_single_coil(1,0) # relay1
+# c.write_single_coil(2,1) # relay2
+# time.sleep(_15minutes)
+
+# # 010
+# print("2")
+# c.write_single_coil(0,0) # relay0
+# c.write_single_coil(1,1) # relay1
+# c.write_single_coil(2,0) # relay2
+# time.sleep(_15minutes)
+
+# # 011
+# print("3")
+# c.write_single_coil(0,0) # relay0
+# c.write_single_coil(1,1) # relay1
+# c.write_single_coil(2,1) # relay2
+# time.sleep(_15minutes)
+
+# # 100
+# print("4")
+# c.write_single_coil(0,1) # relay0
+# c.write_single_coil(1,0) # relay1
+# c.write_single_coil(2,0) # relay2
+# time.sleep(_15minutes)
+
+# # 101
+# print("5")
+# c.write_single_coil(0,1) # relay0
+# c.write_single_coil(1,0) # relay1
+# c.write_single_coil(2,1) # relay2
+# time.sleep(_15minutes)
+
+# # 110
+# print("6")
+# c.write_single_coil(0,1) # relay0
+# c.write_single_coil(1,1) # relay1
+# c.write_single_coil(2,0) # relay2
+# time.sleep(_15minutes)
+
+# # 111
+# print("7")
+# c.write_single_coil(0,1) # relay0
+# c.write_single_coil(1,1) # relay1
+# c.write_single_coil(2,1) # relay2
+# time.sleep(_15minutes)
+
+# print("finish")
+# c.write_single_coil(0,0) # relay0
+# c.write_single_coil(1,0) # relay1
+# c.write_single_coil(2,0) # relay2
+
         # if success display registers
-if bits:
-    print("bit ad #0 to 9: "+str(bits))
+# if bits:
+#     print("bit ad #0 to 9: "+str(bits))
